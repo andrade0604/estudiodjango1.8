cd2350f90727d74ae7e74a640141044a319fbd64
JuanAndrade <andradebj64@gmail.com>
2015-05-27 16:45:32 -0500

    nuevas carpetas

---
+++ acerca de gnu_linux y fedora.py
@@ -0,0 +1,23 @@
+un núcleo monolítico es una arquitectura de sistema operativo donde este en su totalidad trabaja en espacio del núcleo, estando él solo en modo supervisor.
+
+
+Un sistema de gestión de paquetes, también conocido como gestor de paquetes, es una colección de herramientas que sirven para automatizar el proceso de instalación, actualización, configuración y eliminación de paquetes de software.
+
+
+GNU/Linux es uno de los términos empleados para referirse a la combinación del núcleo o kernel libre similar a Unix denominado Linux con el sistema GNU. Su desarrollo es uno de los ejemplos más prominentes de software libre; todo su código fuente puede ser utilizado, modificado y redistribuido libremente por cualquiera bajo los términos de la GPL. 
+
+
+DNF un gestor de paquetes escrito en python y que esta propuesto por el proyecto fedora como el sucesor de YUM y que apartir de Fedora 22, mejor soporte de idiomas y el aumento en la velocidad de rendimiento y menor consumo de memoria
+
+DNF install [paquete]
+DNF update
+DNF check-update
+DNF upgrade
+
+
+Red Hat Inc. es la compañía responsable de la creación y mantenimiento de una distribución del sistema operativo GNU/Linux 
+
+ tres ramas separadas con distintos propósitos, Cloud, Server y Workstation
+
+
+
